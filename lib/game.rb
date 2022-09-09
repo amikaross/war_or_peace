f319af112a5888afb0a6b53c41bdf3497521cb76
Amanda Ross <amikaross@gmail.com>
2022-09-09 15:28:53 -0600

    Refactor: game class and runner

--- lib/game.rb
+++ lib/game.rb
@@ -2,43 +2,77 @@ require "./lib/card.rb"
 require "./lib/deck.rb"
 require "./lib/player.rb"
 require "./lib/turn.rb"
+require "./lib/card_generator.rb"
 
 class Game 
-  def initialize(player1, player2)
-    @player1 = player1 
-    @player2 = player2
+  def initialize
+    @deck1 = card_setup("deck1")
+    @deck2 = card_setup("deck2")
+    @player1 = Player.new("Megan", @deck1)
+    @player2 = Player.new("Aurora", @deck2)
+    @turn_count = 0
+  end
+
+  def intro
+    print "Welcome to War! (or Peace) This game will be played with 52 cards.\nThe players today are Megan and Aurora.\nType 'GO' to start the game!\n------------------------------------------------------------------\n"
+    user_input = gets.chomp.upcase 
+    until user_input == "GO" 
+      user_input = gets.chomp.upcase 
+    end 
+    start
+  end 
+
   end
 
   def start 
-    count = 1
-    until count == 1000001 || @player1.has_lost? || @player2.has_lost? do 
-      turn = Turn.new(@player1, @player2)
-      if turn.type == :mutually_assured_destruction
-        puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
-      elsif turn.type == :war 
-        puts "Turn #{count}: WAR - #{turn.winner.name} won 6 cards"
-      else
-        puts "Turn #{count}: #{turn.winner.name} won 2 cards"
-      end
-      if turn.type == :basic || turn.type == :war 
-        winner = turn.winner 
-        turn.pile_cards 
-        turn.award_spoils(winner)
-      else 
-        turn.pile_cards
-      end
-      count += 1
+    card_setup
+    until game_over? do 
+      play_game
     end 
+    end_game
+  end
+
+
+  def card_setup(deck)
+    all_cards = CardGenerator.new("cards.txt").cards
+    shuffled_once = all_cards.shuffle
+    shuffled_twice = shuffled_once.shuffle
+    shuffled_thrice = shuffled_twice.shuffle
+    cards1 = shuffled_thrice[0..25]
+    cards2 = shuffled_thrice[26..51]
+    if deck == "deck1" then Deck.new(cards1) else Deck.new(cards2) end
+  end 
+
+  def game_over?
+    @turn_count == 1000001 || @player1.has_lost? || @player2.has_lost?
+  end
+
+  def play_game
+    turn = Turn.new(@player1, @player2)
+    if turn.type == :mutually_assured_destruction
+      puts "Turn #{count}: *mutually assured destruction* 6 cards removed from play"
+    elsif turn.type == :war 
+      puts "Turn #{count}: WAR - #{turn.winner.name} won 6 cards"
+    else
+      puts "Turn #{count}: #{turn.winner.name} won 2 cards"
+    end
+    if turn.type == :basic || turn.type == :war 
+      winner = turn.winner 
+      turn.pile_cards 
+      turn.award_spoils(winner)
+    else 
+      turn.pile_cards
+    end
+    @turn_count += 1
+  end
+
+  def end_game 
     if count == 1000001
       puts "---- DRAW ----"
-      return 3
     elsif @player1.has_lost? 
       puts "*~*~*~* #{@player2.name} has won the game! *~*~*~*"
-      return 2
     else 
       puts "*~*~*~* #{@player1.name} has won the game! *~*~*~*"
-      return 1
     end
   end
-end
-
+end 
--- war_or_peace_runner.rb
+++ war_or_peace_runner.rb
@@ -1,41 +1,4 @@
-require "./lib/card"
-require "./lib/deck"
-require "./lib/player"
-require "./lib/turn"
 require "./lib/game"
-require "./lib/card_generator"
 
-
-# Create the standard 52 cards
-all_cards = CardGenerator.new("cards.txt").cards
-
-# shuffle 3 times
-shuffled_once = all_cards.shuffle
-shuffled_twice = shuffled_once.shuffle
-shuffled_thrice = shuffled_twice.shuffle
-
-# split cards into two Decks
-cards1 = shuffled_thrice[0..25]
-cards2 = shuffled_thrice[26..51]
-
-deck1 = Deck.new(cards1)
-deck2 = Deck.new(cards2)
-
-# create two players 
-player1 = Player.new("Megan", deck1)
-player2 = Player.new("Aurora", deck2)
-
-# play game
-print "Welcome to War! (or Peace) This game will be played with 52 cards.\nThe players today are Megan and Aurora.\nType 'GO' to start the game!\n------------------------------------------------------------------\n"
-user_input = gets.chomp 
-count = 0
-until user_input == "GO" || count == 3 do 
-  print "\n"
-  count += 1
-  user_input = gets.chomp 
-end 
-
-if user_input == "GO"
-  game = Game.new(player1, player2)
-  game.start
-end 
+game = Game.new
+game.intro
